0410f35f1d81c261df682783b760d0da6b389342
arjun.rajkumar <arjun.rajkumars@gmail.com>
2020-12-02 11:32:52 +0530

    Making the User model customisable

--- app/models/blogg/article.rb
+++ app/models/blogg/article.rb
@@ -3,13 +3,13 @@ module Blogg
     attr_accessor :author_name
 
     has_many :comments
-    belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blogg.author_class.to_s
  
     before_validation :set_author
 
     private
       def set_author
-        self.author = User.find_or_create_by(name: author_name)
+        self.author = Blogg.author_class.find_or_create_by(name: author_name)
       end
   end
 end
--- lib/blogg.rb
+++ lib/blogg.rb
@@ -1,5 +1,9 @@
 require "blogg/engine"
 
 module Blogg
-  # Your code goes here...
+  mattr_accessor :author_class
+
+  def self.author_class
+    @@author_class.constantize
+  end
 end
