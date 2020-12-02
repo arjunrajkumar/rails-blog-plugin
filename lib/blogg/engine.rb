dac2fdf283bdc2a0d9d4ad8bc3aaa9cc0501f309
arjun.rajkumar <arjun.rajkumars@gmail.com>
2020-12-02 11:44:21 +0530

    Requiring dependencies while autoloading

--- lib/blogg/engine.rb
+++ lib/blogg/engine.rb
@@ -1,5 +1,11 @@
-module Blogg
+module Blorgh
   class Engine < ::Rails::Engine
-    isolate_namespace Blogg
+    isolate_namespace Blorgh
+ 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/overrides/**/*_override*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
